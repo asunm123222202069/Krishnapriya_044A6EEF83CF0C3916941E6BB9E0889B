338406e787e9be612b171a7dfee63f311f5e42af
asunm123222202069 <144411997+asunm123222202069@users.noreply.github.com>
2023-10-27 11:11:27 +0530

    Add files via upload

--- challenges unit 1/challenges1.1.py
+++ challenges unit 1/challenges1.1.py
@@ -1,9 +1,14 @@
+
+
 def recur_factorial(n):
    if n == 1:
        return n
    else:
        return n*recur_factorial(n-1)
+
 num = 7
+
+
 if num < 0:
    print("Sorry, factorial does not exist for negative numbers")
 elif num == 0:
